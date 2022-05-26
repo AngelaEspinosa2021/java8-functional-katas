28d84392823b69ac540732e2d53c5d0066b0fd56
AngelaEspinosa <titagena@gmail.com>
2022-05-26 00:09:30 -0500

    KATA 3: Se logra con ayuda del Flat Mat, acceder al primer nivel de la lista, para poder con ayuda del metodo propio de la clase traernos la lista de videos, y con ayuda del map, logramos captar solo el id de cada video y guardarlo en una nueva lista de enteros.

--- src/main/java/katas/Kata3.java
+++ src/main/java/katas/Kata3.java
@@ -1,11 +1,15 @@
 package katas;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import model.Movie;
 import model.MovieList;
 import util.DataUtil;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
     Goal: Use map() and flatMap() to project and flatten the movieLists into an array of video ids (flatMap(c -> c.stream()))
@@ -16,6 +20,10 @@ public class Kata3 {
     public static List<Integer> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
 
-        return ImmutableList.of(1, 2, 3);
+        List<Integer> listIdsVideos = new ArrayList<>();
+        listIdsVideos = movieLists.stream().flatMap(p-> p.getVideos().stream())
+                .map(Movie::getId).collect(Collectors.toList());
+
+        return listIdsVideos;
     }
 }
