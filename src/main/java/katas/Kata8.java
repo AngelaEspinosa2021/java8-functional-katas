8ec7aacdc05d7584545e7374ce030aa4ad4c9ac1
AngelaEspinosa <titagena@gmail.com>
2022-05-26 02:26:24 -0500

    KATA 8: Con ayuda del ejemplo que nos colocan en el enunciado del ejercicio, se logra hacer la comparacion de las dos listas, la cual es guardada en una tercera lista, y en los parametros, se hace la relacion para que compare y guarde los id a traves de un map.

--- src/main/java/katas/Kata8.java
+++ src/main/java/katas/Kata8.java
@@ -10,6 +10,7 @@ import util.DataUtil;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Combine videos and bookmarks by index (StreamUtils.zip) (https://github.com/poetix/protonpack)
@@ -23,6 +24,8 @@ public class Kata8 {
 
         // StreamUtils.zip()
 
-        return ImmutableList.of(ImmutableMap.of("videoId", 5, "bookmarkId", 3));
+        List<Map> comparator = StreamUtils.zip(movies.stream(), bookMarks.stream(), (a,b) -> ImmutableMap.of("videoId", a.getId(), "bookmarkId", b.getId())).collect(Collectors.toList());
+
+        return comparator;
     }
 }
