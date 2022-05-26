195a183fb9e4d479945c2350847bae273e34371f
AngelaEspinosa <titagena@gmail.com>
2022-05-26 02:35:07 -0500

    KATA 9: Con ayuda de los ejercicios anteriores se logra colocar en una lista tipo Map, los datos que nos piden del video al igual que la instancia del objeto Date y la url de la caja mas pequeña.

--- src/main/java/katas/Kata9.java
+++ src/main/java/katas/Kata9.java
@@ -10,6 +10,7 @@ import util.DataUtil;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve each video's id, title, middle interesting moment time, and smallest box art url
@@ -20,6 +21,14 @@ public class Kata9 {
     public static List<Map> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "some title", "time", new Date(), "url", "someUrl"));
+        List<Map> newMovieList = movieLists.stream().flatMap(p -> p.getVideos().stream())
+                .map(p -> ImmutableMap.of("id", p.getId(), "title", p.getTitle(), "time", new Date(), "url",p.getBoxarts().stream().reduce((a,b)-> {
+                    if(a.getHeight()*a.getHeight() < b.getWidth()*b.getWidth()){
+                        return a;
+                    }
+                    return b;
+                }).map(l -> l.getUrl()))).collect(Collectors.toList());
+
+        return newMovieList;
     }
 }
