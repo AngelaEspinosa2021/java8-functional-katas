e278563bde6ffe047abc8d350a24c82e97a52a75
AngelaEspinosa <titagena@gmail.com>
2022-05-25 23:40:27 -0500

    KATA 1: se logra a traves del uso del map del stream, ejecutar la consulta en un nuevo array.

--- src/main/java/katas/Kata1.java
+++ src/main/java/katas/Kata1.java
@@ -5,8 +5,13 @@ import com.google.common.collect.ImmutableMap;
 import model.Movie;
 import util.DataUtil;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static javax.swing.UIManager.put;
 
 /*
     Goal: use map() to project an array of videos into an array of {id, title}-pairs
@@ -17,6 +22,9 @@ public class Kata1 {
     public static List<Map> execute() {
         List<Movie> movies = DataUtil.getMovies();
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys"));
+        List<Map> newListMovies = new ArrayList<>();
+        newListMovies = movies.stream().map(p -> ImmutableMap.of("Id",p.getId(),"Title", p.getTitle())).collect(Collectors.toList());
+
+        return newListMovies;
     }
 }
